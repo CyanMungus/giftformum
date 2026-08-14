f20f601926ccdd9d447e2adada0e24713b4ee19b
CyanMungus <cyan5925@gmail.com>
2026-08-14 11:33:36 +1000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,7 @@
-// 1) Create a Firebase project.
-// 2) Add a Web App.
-// 3) Paste the firebaseConfig object Firebase gives you below.
-//
-// IMPORTANT:
-// The Firebase web config is meant to be used in browser code.
-// Do NOT put service-account private keys or admin credentials here.
-
 window.MUMMY_HUB_FIREBASE_CONFIG = {
   apiKey: "AIzaSyBHNLyUfEVLYR7S-uMzeTeUYlcMhRZy-ds",
   authDomain: "giftformum-6767.firebaseapp.com",
+  databaseURL: "https://giftformum-6767-default-rtdb.firebaseio.com/",
   projectId: "giftformum-6767",
   storageBucket: "giftformum-6767.firebasestorage.app",
   messagingSenderId: "1089323838150",
