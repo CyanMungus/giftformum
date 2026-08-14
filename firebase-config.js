8a82080cfce021711730c0d1838ddf0607c502e7
CyanMungus <cyan5925@gmail.com>
2026-08-14 11:26:50 +1000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,11 +7,10 @@
 // Do NOT put service-account private keys or admin credentials here.
 
 window.MUMMY_HUB_FIREBASE_CONFIG = {
-  apiKey: "PASTE_YOURS_HERE",
-  authDomain: "PASTE_YOURS_HERE",
-  databaseURL: "PASTE_YOURS_HERE",
-  projectId: "PASTE_YOURS_HERE",
-  storageBucket: "PASTE_YOURS_HERE",
-  messagingSenderId: "PASTE_YOURS_HERE",
-  appId: "PASTE_YOURS_HERE"
+  apiKey: "AIzaSyBHNLyUfEVLYR7S-uMzeTeUYlcMhRZy-ds",
+  authDomain: "giftformum-6767.firebaseapp.com",
+  projectId: "giftformum-6767",
+  storageBucket: "giftformum-6767.firebasestorage.app",
+  messagingSenderId: "1089323838150",
+  appId: "1:1089323838150:web:5cadc80a2234dda33a85c7"
 };
